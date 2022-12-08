06757b5931c924422c4d344a493ad63071752c51
Wenish <Wenish@users.noreply.github.com>
2022-12-08 16:52:58 +0100

    formkit theme with tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { plugin as formkitPlugin, defaultConfig as formkitDefaultConfig} from '@formkit/vue'
 import router from './router'
-import '@formkit/themes/genesis'
 import './index.css'
+import { generateClasses } from '@formkit/themes'
+import theme from './theme'
 
 
 async function bootstrap() {
     const app = createApp(App)
     app.use(createPinia())
     app.use(router)
-    app.use(formkitPlugin, formkitDefaultConfig)
+    app.use(formkitPlugin, formkitDefaultConfig({
+        config: {
+            classes: generateClasses(theme)
+        }
+    }))
     app.mount('#app')
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+const formKitTailwind = require('@formkit/themes/tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    formKitTailwind
+  ],
 }
