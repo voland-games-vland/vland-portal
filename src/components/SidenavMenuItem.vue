22576ce922118b4075b5880744abca0aced4852b
Wenish <Wenish@users.noreply.github.com>
2022-12-12 04:57:27 +0100

    update

--- src/components/SidenavMenuItem.vue
+++ src/components/SidenavMenuItem.vue
@@ -1,6 +1,6 @@
 <template>
 <li>
-    <RouterLink :to="to" class="text-xl group transition-all duration-300 ease-in-out" active-class="active" >
+    <RouterLink :to="to" class="text-xl group transition-all duration-300 ease-in-out" active-class="active">
         <div><slot name="icon"></slot></div>
         <span class="transition-all duration-300 ease-in-out group-hover:pl-3 group-[&.active]:pl-3"><slot name="label"></slot></span>
     </RouterLink>
