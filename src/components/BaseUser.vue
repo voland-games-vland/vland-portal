8768bbeb2299d387324991672de62a3563b851d1
Wenish <Wenish@users.noreply.github.com>
2022-12-12 22:14:55 +0100

    Update BaseUser.vue

--- src/components/BaseUser.vue
+++ src/components/BaseUser.vue
@@ -1,12 +1,7 @@
 <template>
     <div>User:</div>
-    <pre>{{ user }}</pre>
+    <pre>tbd user</pre>
     <div>Clan: <RouterLink to="/clan">Blaze Hood</RouterLink></div>
-    <input v-model="user?.displayName" />
 </template>
 <script lang="ts" setup>
-
-import { getAuth } from 'firebase/auth'
-import { useAuth } from '@vueuse/firebase/useAuth'
-const { user } = useAuth(getAuth())
 </script>
