7e10dda2ae958c8f271ec6fa9723951211be000f
Wenish <Wenish@users.noreply.github.com>
2022-12-20 21:30:57 +0100

    Update PageStart.vue

--- src/pages/PageStart.vue
+++ src/pages/PageStart.vue
@@ -3,7 +3,7 @@
         <div class="prose">
             <h1>Hello {{ user?.email }}</h1>
             <h2>Welcome to V-Land</h2>
-            <p>V-Land is a Sandbox game, where you build and play your own maps.</p>
+            <p>V-Land is a Sandbox MOBA game, where you build and play your own maps.</p>
         </div>
     </div>
 </template>
