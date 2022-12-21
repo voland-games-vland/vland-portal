7891e51a659dd6ca5d92f1b8d64eac77b0c8774c
Wenish <Wenish@users.noreply.github.com>
2022-12-21 02:06:42 +0100

    Update myMaps.store.ts

--- src/stores/myMaps.store.ts
+++ src/stores/myMaps.store.ts
@@ -1,11 +1,13 @@
+import { getAuth } from 'firebase/auth'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import vlandApi, { CreateMapDto, Map } from '../apis/vland.api'
+import vlandApi, { Map } from '../apis/vland.api'
 import { useUserStore } from './user.store'
 
 export const useMyMapsStore = defineStore(
   'myMaps',
   () => {
+    const auth = getAuth()
     const userStore = useUserStore()
 
     const maps = ref<Map[]>([])
@@ -15,6 +17,12 @@ export const useMyMapsStore = defineStore(
         if (!userId) return
         maps.value = await vlandApi.users.id.maps.get(userId)
     }
+
+    auth.onAuthStateChanged(async (authUser) => {
+        if(!authUser) {
+            maps.value = []
+        }
+      })
 
     return {
         maps,
