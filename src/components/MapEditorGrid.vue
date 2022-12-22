77ebf781228e1fe4dd9289310db6db0cd7db0917
Wenish <Wenish@users.noreply.github.com>
2022-12-22 21:00:25 +0100

    Update MapEditorGrid.vue

--- src/components/MapEditorGrid.vue
+++ src/components/MapEditorGrid.vue
@@ -1,11 +1,18 @@
 <template>
-    <div
-        class="grid gap-px bg-black border border-black"
-        :style="gridStyle"
-    >
-        <div v-for="index in mapEditGridStore.gridItemsCount" :key="index" class="bg-slate-300 hover:bg-slate-400 overflow-hidden relative">
-            <div class="font-thin absolute bottom-0 right-0 select-none text-[9px] text-gray-600">{{ mapEditGridStore.getAxisFromIndex(index).x }},{{ mapEditGridStore.getAxisFromIndex(index).z }}</div>
+    <div class="grid gap-px bg-black border border-black" :style="getGridStyle">
+        <div v-for="index in mapEditGridStore.gridItemsCount" :key="index" :style="getGridItemStyles(index)"
+            class="bg-slate-300 hover:opacity-75 overflow-hidden relative"
+            :class="['grid-item', getGridItemTypeClass(index)]">
+            <div class="font-thin absolute bottom-0 right-0 select-none text-[9px] text-gray-600">{{
+                    mapEditGridStore.getAxisFromIndex(index).x
+            }},{{ mapEditGridStore.getAxisFromIndex(index).z }}</div>
         </div>
+        <!--
+            <div v-for="(block, index) in mapEditGridStore.blocks" :key="index" :style="{
+                gridColumn: block.position.z,
+                gridRow: block.position.x,
+            }"></div>
+        -->
     </div>
 </template>
 <script lang="ts" setup>
@@ -17,10 +24,53 @@ const width = computed(() => mapEditGridStore.gridWidth)
 const height = computed(() => mapEditGridStore.gridHeigth)
 const gridItemSize = computed(() => mapEditGridStore.gridItemSize)
 
-const gridStyle = computed(() => {
+const getGridStyle = computed(() => {
     return {
         gridTemplateColumns: `repeat(${width.value}, ${gridItemSize.value}px)`,
         gridTemplateRows: `repeat(${height.value}, ${gridItemSize.value}px)`,
     };
 });
+
+const getGridItemStyles = (index: number) => {
+    const axis = mapEditGridStore.getAxisFromIndex(index);
+    return {
+        gridColumn: axis.z,
+        gridRow: axis.x,
+    };
+};
+
+const getGridItemTypeClass = (index: number) => {
+    const axis = mapEditGridStore.getAxisFromIndex(index);
+    const key = `${axis.x}_0_${axis.z}`;
+    const block = mapEditGridStore.blocks[key]
+    return !!block
+        ? block.type.toLocaleLowerCase()
+        : ''
+};
 </script>
+<style scoped>
+.grid-item.stone {
+    @apply bg-gray-500;
+}
+.grid-item.grass {
+    @apply bg-green-500;
+}
+.grid-item.dirt {
+    @apply bg-amber-900;
+}
+.grid-item.wood {
+    @apply bg-amber-700;
+}
+.grid-item.bridge {
+    @apply bg-orange-300;
+}
+.grid-item.snow {
+    @apply bg-yellow-50;
+}
+.grid-item.sand {
+    @apply bg-yellow-200;
+}
+.grid-item.water {
+    @apply bg-blue-500;
+}
+</style>
