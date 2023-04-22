6f0acdad4b17f1cfd8a37b88f8f54ea15f1bc1c0
Wenish <Wenish@users.noreply.github.com>
2023-04-23 01:46:42 +0200

    Create mapEditorBuildingbar.store.ts

---
+++ src/stores/mapEditorBuildingbar.store.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { BUILDING } from '../apis/vland.api'
+
+export const useMapEditorBuildingbarStore = defineStore(
+  'mapEditorBuildingbar',
+  () => {
+    const buildings = ref<BUILDING[]>([
+        BUILDING.Spawn,
+        BUILDING.CapturePoint,
+        BUILDING.Teleporter,
+        BUILDING.Spawner,
+    ])
+    const selectedBuilding = ref<BUILDING>(BUILDING.Spawn)
+
+    return {
+        buildings,
+        selectedBuilding
+    }
+  },
+  {
+    persist: {
+      paths: ['selectedBuilding']
+    }
+  }
+)
