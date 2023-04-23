ed1d61c80154321b177c63914cc0a41dd3c648b4
Wenish <Wenish@users.noreply.github.com>
2023-04-23 03:22:09 +0200

    Update UserBadgeLevel.vue

--- src/components/UserBadgeLevel.vue
+++ src/components/UserBadgeLevel.vue
@@ -1,5 +1,6 @@
 <template>
-    <div v-if="level" class="level-badge inline-grid bg-neutral text-neutral-content place-items-center py-1 px-4 ml-1 text-sm">
+    <div v-if="level"
+        class="level-badge inline-grid bg-neutral text-neutral-content place-items-center py-1 px-4 ml-2 text-sm border-b-2 border-x-2 border-neutral-focus">
         {{ level }}
     </div>
 </template>
