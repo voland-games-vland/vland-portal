b61d8bcdaa06b8e5ada5755784d113085b73c231
Wenish <Wenish@users.noreply.github.com>
2023-04-25 17:19:20 +0200

    Update PagePlaybook.vue

--- src/pages/PagePlaybook.vue
+++ src/pages/PagePlaybook.vue
@@ -2,8 +2,9 @@
     <div class="p-4 grid gap-4">
         <NavigationBack toBack="/start">Playbook</NavigationBack>
         <article class="prose">
-            <h1>Guide</h1>
+            <h1>V-Land Guide</h1>
             <p>In this guide the content, mechanics and rules of the game are going to be explained.</p>
+            <p>V-Land is a Sandbox MOBA in shortform Saba, where you build and play your own maps.</p>
             <!-- ... -->
         </article>
     </div>
