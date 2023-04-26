f70723914d6acb83bf154f3beb4f7ebdd3c6b918
Wenish <Wenish@users.noreply.github.com>
2023-04-26 04:38:01 +0200

    Update PagePaymentCreditCard.vue

--- src/pages/PagePaymentCreditCard.vue
+++ src/pages/PagePaymentCreditCard.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="title">SELECT RACE CONTROL POINTS</div>
-    <div class="grid-products-wrapper">
-        <div class="grid-products">
+  <div class="p-4 grid gap-4">
+    <NavigationBack toBack="/start">Payment - Credit Card</NavigationBack>
+    <div class="text-4xl font-bold text-center">SELECT V-Land POINTS</div>
+    <div class="max-w-5xl mx-auto">
+        <div class="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
             <div v-for="product in products">
                 <suspense>
                     <template #default>
@@ -12,94 +14,55 @@
             </div>
         </div>
     </div>
+  </div>
 </template>
-<script lang="ts">
-import { defineAsyncComponent, defineComponent, ref } from "vue";
+<script lang="ts" setup>
+import { defineAsyncComponent, ref } from "vue";
 
-export default defineComponent({
-  name: "PagePoints",
-  components: {
-    RcProduct: defineAsyncComponent(() => import('../components/RcProduct.vue')),
+const NavigationBack = defineAsyncComponent(() => import('../components/NavigationBack.vue'))
+const RcProduct = defineAsyncComponent(() => import('../components/RcProduct.vue'))
+
+const products = ref([
+  {
+    amount: 500,
+    name: '475',
+    description: '475 V-Land Points + 0 bonus V-Land Points',
+    id: 'prod_KgJ30IByFKidYr'
   },
-  setup() {
-    const products = ref([
-      {
-        amount: 500,
-        name: '475',
-        description: '475 RACE CONTROL Points + 0 bonus RACE CONTROL Points',
-        id: 'prod_KgJ30IByFKidYr'
-      },
 
-      {
-        amount: 1000,
-        name: '1000',
-        description: '950 RACE CONTROL Points + 50 bonus RACE CONTROL Points',
-        id: 'prod_KgJ440e8f81VCO'
-      },
+  {
+    amount: 1000,
+    name: '1000',
+    description: '950 V-Land Points + 50 bonus V-Land Points',
+    id: 'prod_KgJ440e8f81VCO'
+  },
 
-      {
-        amount: 2000,
-        name: '2050',
-        description: '1900 RACE CONTROL Points + 150 bonus RACE CONTROL Points',
-        id: 'prod_KgJ4oaAsM0p4Vy'
-      },
+  {
+    amount: 2000,
+    name: '2050',
+    description: '1900 V-Land Points + 150 bonus V-Land Points',
+    id: 'prod_KgJ4oaAsM0p4Vy'
+  },
 
-      {
-        amount: 3500,
-        name: '3650',
-        description: '3330 RACE CONTROL Points + 320 bonus RACE CONTROL Points',
-        id: 'prod_KgJ6wwUoa9iYIN'
-      },
+  {
+    amount: 3500,
+    name: '3650',
+    description: '3330 V-Land Points + 320 bonus V-Land Points',
+    id: 'prod_KgJ6wwUoa9iYIN'
+  },
 
-      {
-        amount: 5000,
-        name: '5350',
-        description: '4750 RACE CONTROL Points + 600 bonus RACE CONTROL Points',
-        id: 'prod_KgJ8QPHpL2bmDZ'
-      },
+  {
+    amount: 5000,
+    name: '5350',
+    description: '4750 V-Land Points + 600 bonus V-Land Points',
+    id: 'prod_KgJ8QPHpL2bmDZ'
+  },
 
-      {
-        amount: 10000,
-        name: '11000',
-        description: '9500 RACE CONTROL Points + 1500 bonus RACE CONTROL Points',
-        id: 'prod_KgJ9lnd2qHVlHj'
-      }
-    ])
-    return {
-      products
-    }
+  {
+    amount: 10000,
+    name: '11000',
+    description: '9500 V-Land Points + 1500 bonus V-Land Points',
+    id: 'prod_KgJ9lnd2qHVlHj'
   }
-});
+])
 </script>
-
-<style scoped>
-.title {
-  font-size: large;
-  font-weight: bold;
-  padding: 20px;
-  color: #edf6fe;
-}
-.grid-products-wrapper {
-  max-width: 1000px;
-  margin: auto;
-}
-
-.grid-products {
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: 1fr;
-  /* grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); */
-}
-
-@media only screen and (min-width: 600px) {
-  .grid-products {
-    grid-template-columns: 1fr 1fr;
-  }
-}
-
-@media only screen and (min-width: 900px) {
-  .grid-products {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-}
-</style>
