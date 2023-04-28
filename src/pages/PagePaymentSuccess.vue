b0a473f3b0de7f325e244ff896f681945b7a78c1
Wenish <Wenish@users.noreply.github.com>
2023-04-28 17:33:27 +0200

    Update PagePaymentSuccess.vue

--- src/pages/PagePaymentSuccess.vue
+++ src/pages/PagePaymentSuccess.vue
@@ -2,20 +2,22 @@
     <div class="p-4 grid gap-2">
         <NavigationBack toBack="/start">Payment Success</NavigationBack>
         <div class="text-center text-4xl font-bold">Payment via Stripe Success</div>
-        <div class="text-2xl flex justify-center items-center gap-2">
+        <div class="text-2xl flex justify-center items-center gap-2" v-if="pointsTotal">
             <span>Received</span>
             <span><IconBill width="50" height="50" /></span>
-            <span class="text-3xl text-green-500 font-bold">{{ amount }}</span>
+            <span class="text-3xl text-green-500 font-bold">{{ pointsTotal }}</span>
             <span>V-Land Points.</span>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
 import { useRouteQuery } from "@vueuse/router";
-import { defineAsyncComponent } from "vue";
+import { computed, defineAsyncComponent } from "vue";
 
 const NavigationBack = defineAsyncComponent(() => import('../components/NavigationBack.vue'))
 const IconBill = defineAsyncComponent(() => import('../components/IconBill.vue'))
 
-const amount = useRouteQuery<string>('amount', undefined)
+const points = useRouteQuery<string>('points', undefined)
+const pointsBonus = useRouteQuery<string>('pointsBonus', undefined)
+const pointsTotal = computed(() => Number(points.value) + Number(pointsBonus.value))
 </script>
