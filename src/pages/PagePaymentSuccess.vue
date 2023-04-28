f237cf2aa942d9bbadf9fd8cdfd635b60f4ab325
Wenish <Wenish@users.noreply.github.com>
2023-04-28 17:18:47 +0200

    Update PagePaymentSuccess.vue

--- src/pages/PagePaymentSuccess.vue
+++ src/pages/PagePaymentSuccess.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-4 grid gap-2">
-        <NavigationBack toBack="/start">Payment Success</NavigationBack>
+        <NavigationBack toBack="/market">Payment Success</NavigationBack>
         <div class="text-center text-4xl font-bold">Payment via Stripe Success</div>
         <div class="text-2xl flex justify-center items-center gap-2">
             <span>Received</span>
