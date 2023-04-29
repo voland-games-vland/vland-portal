bcd230d3be4e54c3c4c8e90d6fa9c95580946e85
Wenish <Wenish@users.noreply.github.com>
2023-04-29 03:13:12 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
 <template>
   <router-view />
+  <div class="absolute top-0 w-full pointer-events-none z-50 grid justify-items-center opacity-70">
+    <div class="bg-red-600 text-white text-xs font-bold py-0.5 px-1">PRE-ALPHA</div>
+  </div>
 </template>
