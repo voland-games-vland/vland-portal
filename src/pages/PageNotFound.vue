8d0f2f09d9c3e725ed8ddaebaefd4dae1ac757c4
Wenish <Wenish@users.noreply.github.com>
2023-04-30 18:33:03 +0200

    Update PageNotFound.vue

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,15 +1,11 @@
 <template>
-    <div class="container">
-        <div>Page Not Found</div>
-        <RouterLink to="/start">Back to Home</RouterLink>
+    <div class="grid justify-center p-4 gap-2">
+        <h1 class="text-4xl font-bold">Page Not Found</h1>
+        <div class="grid justify-center">
+            <RouterLink to="/start" class="underline hover:text-primary-focus">Back to Home</RouterLink>
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
 import { RouterLink } from 'vue-router';
 </script>
-<style scoped>
-.container {
-    display: grid;
-    justify-content: center;
-}
-</style>
