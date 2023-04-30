74066e6b337551a7215f44191274053a57518a38
Wenish <Wenish@users.noreply.github.com>
2023-04-30 18:24:45 +0200

    Update PageHome.vue

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="grid">
-        <div class="left">
+    <div class="grid grid-cols-[auto_1fr] grid-rows-[1fr] h-full">
+        <div class="w-80 max-w-80 p-8">
             <div class="text-4xl uppercase pb-4 font-bold text-center">V-Land</div>
             <br />
             <router-view></router-view>
         </div>
-        <div class="right">
+        <div class="right bg-center bg-no-repeat bg-cover">
         </div>
     </div>
 </template>
@@ -13,26 +13,8 @@
 </script>
 <style scoped>
 
-.grid {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    grid-template-rows: 1fr;
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
-    height: 100%;
-}
-
 .right {
     box-shadow: 0px 0px 3px 1px #000000;
     background-image: url('../assets/artwork1.jpeg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-
-.left {
-    max-width: 300px;
-    width: 300px;
-    padding: 2rem;
 }
 </style>
