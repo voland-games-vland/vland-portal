90dbae399d37081f47a9f12da45652f82c6ef98a
Wenish <Wenish@users.noreply.github.com>
2023-05-02 02:30:17 +0200

    refactor

---
+++ src/components/CharacterListItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="group grid gap-2 bg-slate-900 text-slate-300 hover:text-slate-50 p-4 text-center border-4 border-slate-700 hover:bg-slate-800 hover:border-slate-800 transition-all duration-300">
+        <div class="font-bold text-xl truncate">{{ name }}</div>
+        <div class="flex justify-center">
+            <div class="w-12 h-12 bg-slate-500 truncate text-xs font-bold flex items-center justify-center">
+                <div class="rotate-45">{{ weaponType }}</div>
+            </div>
+        </div>
+    </button>
+</template>
+<script setup lang="ts">
+defineProps<{
+    id: string,
+    name: string,
+    weaponType: string
+}>()
+</script>
--- src/pages/PageCharacters.vue
+++ src/pages/PageCharacters.vue
@@ -3,25 +3,25 @@
         <NavigationBack toBack="/start">Characters</NavigationBack>
         <ButtonNewCharacter />
         <div class="grid grid-cols-[repeat(auto-fill,minmax(242px,1fr))] gap-4">
-            <button v-for="character in charactersMyStore.characters" class="group grid gap-2 bg-slate-900 text-slate-300 hover:text-slate-50 p-4 text-center border-4 border-slate-700 hover:bg-slate-800 hover:border-slate-800 transition-all duration-300">
-                <div class="font-bold text-xl truncate">{{ character.name }}</div>
-                <div class="flex justify-center">
-                    <div class="w-12 h-12 bg-slate-500 truncate text-xs font-bold flex items-center justify-center"><div class="rotate-45">{{ character.weaponType }}</div></div>
-                </div>
-            </button>
+            <CharacterListItem
+                v-for="character in charactersMyStore.characters"
+                :key="character._id"
+                :id="character._id"
+                :name="character.name"
+                :weaponType="character.weaponType"
+            />
         </div>
     </div>
 </template>
 <script lang="ts" setup>
-import { defineAsyncComponent, ref } from 'vue';
+import { defineAsyncComponent } from 'vue';
 import { useCharactersMyStore } from '../stores/charactersMy.store';
 
 const NavigationBack = defineAsyncComponent(() => import('../components/NavigationBack.vue'))
-const IconPlus = defineAsyncComponent(() => import('../components/IconPlus.vue'))
 const ButtonNewCharacter = defineAsyncComponent(() => import('../components/ButtonNewCharacter.vue'))
+const CharacterListItem = defineAsyncComponent(() => import('../components/CharacterListItem.vue'))
 
 const charactersMyStore = useCharactersMyStore()
-
 
 charactersMyStore.loadMyCharacters()
 </script>
