2792ae6e5a5662a461c61f74d472449a8ed025e8
Wenish <Wenish@users.noreply.github.com>
2023-05-02 20:19:59 +0200

    Update PageCharactersEdit.vue

--- src/pages/PageCharactersEdit.vue
+++ src/pages/PageCharactersEdit.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="p-4 grid gap-4 ">
+    <div class="p-4 grid gap-4">
         <NavigationBack toBack="/characters">Characters_Edit</NavigationBack>
-        <div class="grid w-full max-w-xs mx-auto">
+        <div class="grid w-full max-w-xs mx-auto" v-if="!isPageLoading">
             <FormCharacterEdit v-model:data="formData" />
+        </div>
+        <div class="grid" v-if="isPageLoading">
+            <div v-if="isPageLoading" class="text-center font-bold m-auto grid gap-2">
+                <p>Loading Character Data</p>
+                <SpinnerRound />
+            </div>
         </div>
     </div>
 </template>
@@ -13,8 +19,10 @@ import vlandApi, { Character, Weapon } from '../apis/vland.api';
 
 const NavigationBack = defineAsyncComponent(() => import('../components/NavigationBack.vue'))
 const FormCharacterEdit = defineAsyncComponent(() => import('../components/FormCharacterEdit.vue'))
+const SpinnerRound = defineAsyncComponent(() => import('../components/SpinnerRound.vue'))
 const characterId = useRouteParams<string>('id')
 const character = ref<Character | null>(null)
+const isPageLoading = ref(true)
 
 const formData = ref({
     name: '',
@@ -22,10 +30,12 @@ const formData = ref({
 })
 
 const loadCharacter = async () => {
+    isPageLoading.value = true
     const response = await vlandApi.characters.id.get(characterId.value)
     character.value = response
     formData.value.name = response.name
     formData.value.weaponType = response.weaponType as Weapon
+    isPageLoading.value = false
 }
 
 loadCharacter()
