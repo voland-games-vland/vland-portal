f87985e1261d7ee8be09e9ad9cef9759437c7647
Wenish <Wenish@users.noreply.github.com>
2023-05-02 20:13:40 +0200

    feat: form character edit

--- src/components/FormCharacterEdit.vue
+++ src/components/FormCharacterEdit.vue
@@ -1,6 +1,72 @@
 <template>
-    <div class="grid gap-2">
-        <h2 class="font-bold text-2xl">Edit Character</h2>
-    </div>
+    <FormKit type="form" :actions="false" @submit="$emit('submit')">
+        <FormKit
+            :disabled="isSaving"
+            type="text"
+            name="name"
+            label="Name"
+            v-model.trim="formData.name"
+            validation="required|length:3,30"
+        />
+        <FormKit
+            :disabled="isSaving"
+            type="select"
+            label="Weapon Type"
+            placeholder="Select a Weapon Type"
+            name="weaponType"
+            v-model="formData.weaponType"
+            :options="weaponTypeOptions"
+            validation="required"
+        />
+        <div class="flex gap-2">
+            <button type="button" :disabled="isSaving" class="btn btn-error btn-outline" @click="$emit('delete')">
+                Delete
+            </button>
+            <button type="submit" class="btn btn-primary" :class="{ loading: isSaving}">
+                Save
+            </button>
+        </div>
+    </FormKit>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, defineAsyncComponent, ref } from 'vue';
+import { Weapon } from '../apis/vland.api';
+import { FormKit } from '@formkit/vue';
+
+const props = withDefaults(defineProps<{
+    data?: {
+        name: string,
+        weaponType: Weapon
+    },
+    isSaving?: boolean
+}>(),{
+    data: () => {
+        return {
+            name: '',
+            weaponType: Weapon.Sword
+        }
+    },
+    isSaving: false
+})
+
+const weaponTypeOptions = ref<Weapon[]>([
+    Weapon.Sword,
+    Weapon.Bow,
+    Weapon.Daggers,
+    Weapon.Hammer,
+    Weapon.Spear,
+    Weapon.Pistols,
+    Weapon.Sniper
+])
+
+const emit = defineEmits<{
+    (e: 'update:data', data: typeof props.data): void,
+    (e: 'submit'): void,
+    (e: 'delete'): void
+}>()
+
+const formData = computed({
+    get: () => props.data,
+    set: (value) => emit('update:data', value)
+})
+</script>
--- src/pages/PageCharactersEdit.vue
+++ src/pages/PageCharactersEdit.vue
@@ -1,11 +1,32 @@
 <template>
-    <div class="p-4 grid gap-4">
+    <div class="p-4 grid gap-4 ">
         <NavigationBack toBack="/characters">Characters_Edit</NavigationBack>
-        Page Char Edit
+        <div class="grid w-full max-w-xs mx-auto">
+            <FormCharacterEdit v-model:data="formData" />
+        </div>
     </div>
 </template>
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue';
+import { useRouteParams } from '@vueuse/router';
+import { defineAsyncComponent, ref } from 'vue';
+import vlandApi, { Character, Weapon } from '../apis/vland.api';
 
 const NavigationBack = defineAsyncComponent(() => import('../components/NavigationBack.vue'))
+const FormCharacterEdit = defineAsyncComponent(() => import('../components/FormCharacterEdit.vue'))
+const characterId = useRouteParams<string>('id')
+const character = ref<Character | null>(null)
+
+const formData = ref({
+    name: '',
+    weaponType: Weapon.Sword
+})
+
+const loadCharacter = async () => {
+    const response = await vlandApi.characters.id.get(characterId.value)
+    character.value = response
+    formData.value.name = response.name
+    formData.value.weaponType = response.weaponType as Weapon
+}
+
+loadCharacter()
 </script>
