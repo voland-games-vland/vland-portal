ef8a3a6f374f8414eab5b3390758438537ef7903
Wenish <Wenish@users.noreply.github.com>
2023-05-04 02:04:46 +0200

    Update AttributeSpenderBar.vue

--- src/components/AttributeSpenderBar.vue
+++ src/components/AttributeSpenderBar.vue
@@ -1,8 +1,13 @@
 <template>
 <div class="grid">
-    <div class="text-sm">{{ label }}</div>
+    <div class="text-sm">{{ label }} + <span class="font-bold">{{ value }}</span></div>
     <div class="inline-flex gap-1 items-center">
-        <progress class="progress w-full" :value="value" :max="max"></progress>
+        <div class="w-full relative">
+            <progress class="progress w-full" :value="value" :max="max"></progress>
+            <div class="w-full flex justify-between text-xs absolute top-[calc(50%-0.1rem)]">
+                <span class="w-[1px] h-2 bg-primary-focus first:bg-transparent last:bg-transparent" v-for="index in max + 1"></span>
+            </div>
+        </div>
         <button type="button" class="btn btn-square btn-xs" @click="$emit('subtract')" :disabled="!value">-</button>
         <button type="button" class="btn btn-square btn-xs" @click="$emit('add')" :disabled="disabledAdd">+</button>
     </div>
